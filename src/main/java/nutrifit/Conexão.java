cec149078b6ab1eab98793272b50c41861ab0048
Otavio <802.263@alunos.unigran.br>
2023-06-14 13:31:58 -0400

    Conexão com o banco de dados

---
+++ src/main/java/nutrifit/Conexão.java
@@ -0,0 +1,30 @@
+package nutrifit;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Conexão {
+
+    public static void initialize() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("bdd-tcc-firebase-adminsdk-pzbgn-790d3603b8.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+            
+            System.out.println("Firebase inicializado com sucesso!");
+        } catch (FileNotFoundException e) {
+            System.err.println("Arquivo de chave não encontrado: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo de chave: " + e.getMessage());
+        }
+    }
+
+}
--- src/main/java/nutrifit/NutriFit.java
+++ src/main/java/nutrifit/NutriFit.java
@@ -3,6 +3,6 @@ package nutrifit;
 public class NutriFit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Conexão.initialize();
     }
 }
